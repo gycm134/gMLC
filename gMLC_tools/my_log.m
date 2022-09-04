4cb2eb6568fc130af3350de562513b7780b7acbc
Guy Y. Cornejo Maceda <54544791+gycm134@users.noreply.github.com>
2022-09-04 14:43:30 +0800

    Copyright updated

--- gMLC_tools/my_log.m
+++ gMLC_tools/my_log.m
@@ -1,12 +1,11 @@
 function q=my_log(x)
     % MY_LOG customized version of log
     %
-    % Guy Y. Cornejo Maceda, 01/24/2020
     %
     % See also my_div, my_log, opset.
 
-    % Copyright: 2020 Guy Cornejo Maceda (gy.cornejo.maceda@gmail.com)
-    % CC-BY-SA
+    % Copyright (C) 2015-2019 Thomas Duriez.
+    % This file is part of the OpenMLC-Matlab-2 Toolbox. Distributed under GPL v3.
 
 protection = 1e-3;
 q=log10(abs(x).*(abs(x)>=protection)+protection*(abs(x)<protection));
